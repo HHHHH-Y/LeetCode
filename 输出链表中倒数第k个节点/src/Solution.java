517d20d590342a7340592fee71e68637f5c5f2e7
HHHHH-Y <984495875@qq.com>
2020-04-26 11:24:49 +0800

    2020/4/26

--- 输出链表中倒数第k个节点/src/Solution.java
+++ 输出链表中倒数第k个节点/src/Solution.java
@@ -13,27 +13,22 @@ import java.util.List;
 }
 public class Solution {
     public ListNode getKthFromEnd(ListNode head, int k) {
-        ListNode fast = head;
-        ListNode slow = head;
-        // 判断k的位置是否合法
-        ListNode cur = head;
-        int size = 0;
-        while(cur != null) {
-            size += 1;
-            cur = cur.next;
-        }
-        if(k < 1 || k > size) {
+        // 1. 判断单链表是否为空, 若为空, 则没有倒数第k个节点
+        // 2. 要对k进行合法性判断
+        // 3. 定义两个引用, 一个先走k-1步
+        //    然后两个引用一起走, 直到先走的引用到达最后一个节点为止
+        if(k < 1) {
             return null;
         }
-        int count = k - 1;
+        ListNode fast = head;
+        ListNode slow = head;
+        for (int i = 0; i < k - 1; i++) {
+            fast = fast.next;
+            if(fast == null) {
+                return null;
+            }
+        }
         while (fast.next != null) {
-            while (count != 0) {
-                fast = fast.next;
-                count--;
-            }
-            if(fast.next == null) {
-                return slow;
-            }
             fast = fast.next;
             slow = slow.next;
         }
