fec92875fe26375926da173b4e03c0a51f10f2ee
HHHHH-Y <984495875@qq.com>
2020-06-02 21:33:37 +0800

    删除排序数组中的重复项

--- 删除排序数组中的重复项/src/Demo.java
+++ 删除排序数组中的重复项/src/Demo.java
@@ -1,4 +1,4 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 /**
  * Created with IntelliJ IDEA.
@@ -6,5 +6,29 @@ package PACKAGE_NAME;
  * User: HHH.Y
  * Date: 2020-04-20
  */
+
+/**
+ * 给定一个 haystack 字符串和一个 needle 字符串
+ * 在 haystack 字符串中找出 needle 字符串出现的第一个位置 (从0开始)。如果不存在，则返回  -1。
+ */
+class Solution {
+    public int strStr(String haystack, String needle) {
+        if(needle.length() == 0) {
+            return 0;
+        }
+        for (int i = 0; i < haystack.length(); i++) {
+           
+        }
+    }
+}
 public class Demo {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        String haystack = "hello";
+        String needle = "ll";
+        /*Scanner scanner = new Scanner(System.in);
+        String haystack = scanner.nextLine();
+        String needle = scanner.nextLine();*/
+        System.out.println(solution.strStr(haystack, needle));
+    }
 }
