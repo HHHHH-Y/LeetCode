1b1f100fc488fdfc0166d9ed958dca17558f93d5
HHHHH-Y <984495875@qq.com>
2020-06-08 00:38:47 +0800

    删除公共字符问题, 代码更新

--- 每日一题/day04/src/TestDemo2.java
+++ 每日一题/day04/src/TestDemo2.java
@@ -11,24 +11,45 @@ import java.util.Scanner;
  * Date: 2020-06-04
  */
 public class TestDemo2 {
-    public static void deleteSame(String str, String s) {
-        List<Character> list = new ArrayList<>();
-        for (int i = 0; i < s.length(); i++) {
-            list.add(s.charAt(i));
-        }
-        StringBuffer res= new StringBuffer();
-        for (int i = 0; i < str.length(); i++) {
-            if(!list.contains(str.charAt(i))) {
-                res.append(str.charAt(i));
-            }
-        }
-        String showString = res.toString();
-        System.out.println(showString);
-    }
+//    public static void deleteSame(String str, String s) {
+//        List<Character> list = new ArrayList<>();
+//        for (int i = 0; i < s.length(); i++) {
+//            list.add(s.charAt(i));
+//        }
+//        StringBuffer res= new StringBuffer();
+//        for (int i = 0; i < str.length(); i++) {
+//            if(!list.contains(str.charAt(i))) {
+//                res.append(str.charAt(i));
+//            }
+//        }
+//        String showString = res.toString();
+//        System.out.println(showString);
+//    }
+//    public static void main(String[] args) {
+//        Scanner scanner = new Scanner(System.in);
+//        String str = scanner.nextLine();
+//        String s = scanner.nextLine();
+//        deleteSame(str, s);
+//    }
+
+    // 如何判断字符串中是否包含某个非字符串的元素, 使用str.contains(String.valueOf(...))即可
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String str = scanner.nextLine();
-        String s = scanner.nextLine();
-        deleteSame(str, s);
+        while (scanner.hasNext()) {
+//            char[] ch = scanner.nextLine().toCharArray(); // They are students
+//            String str = scanner.nextLine(); // aeiou
+//            for (int i = 0; i < ch.length; i++) {
+//                if(!str.contains(String.valueOf(ch[i]))) {
+//                    System.out.print(ch[i]);
+//                }
+//            }
+            String str = scanner.nextLine();
+            String s = scanner.nextLine();
+            for (int i = 0; i < str.length(); i++) {
+                if(!s.contains(String.valueOf(str.charAt(i)))) {
+                    System.out.print(str.charAt(i));
+                }
+            }
+        }
     }
 }
