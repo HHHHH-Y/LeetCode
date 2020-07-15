c78ae8552cbb91fcd2cd498337f48edad366d06d
HHHHH-Y <984495875@qq.com>
2020-07-15 11:42:25 +0800

    二维数组的打印

--- 每日一题/day38/src/Printer.java
+++ 每日一题/day38/src/Printer.java
@@ -8,6 +8,22 @@
  */
 public class Printer {
     public int[] arrayPrint(int[][] arr, int n) {
-        return null;
+        int[] out = new int[n * n];
+        // 打印上三角包括中间 00 - nn 的对角线
+        int index = 0;
+        for (int j = n - 1; j >= 0; j--) {
+            int k = j;
+            for (int i = 0; i < n - j; i++) {
+                out[index++] = arr[i][k++];
+            }
+        }
+        // 打印剩下的下三角
+        for (int i = 1; i < n; i++) {
+            int k = i;
+            for (int j = 0; j < n - i; j++) {
+                out[index++] = arr[k++][j];
+            }
+        }
+        return out;
     }
 }
