bd70cd1651d499e2db37a4ebf27fb34f390e881d
HHHHH-Y <984495875@qq.com>
2020-07-21 11:07:17 +0800

    使用正则表达式的方式分割字符串求解单词倒排问题

--- 每日一题/day43/src/InvertedOrderOfWords.java
+++ 每日一题/day43/src/InvertedOrderOfWords.java
@@ -10,8 +10,8 @@ import java.util.Scanner;
  * 如果原字符串中相邻单词间有多个间隔符时, 倒排转换后也只允许出现一个空格间隔符
  *
  * 思路: 首先将原字符串通过非字母(正则表达式)进行划分
- * 然后从后向前遍历打印
- * 如果字符串数组中下标为 0 的元素时空串, 则从小标为 0 的元素结束, 否则, 就要输出下标为 1 的元素和下标为 0 的元素
+ * 然后从后向前遍历并放入到 StringBuffer 中
+ * 最后的结果去掉两边的空格即可
  * User: HHH.Y
  * Date: 2020-07-21
  */
@@ -25,15 +25,12 @@ public class InvertedOrderOfWords {
     }
 
     private static void outputWords(String words) {
+        StringBuffer stringBuffer = new StringBuffer();
         // 通过正则表达式, 以非字符串进行分割
         String[] split = words.split("[^a-zA-Z]+");
-        for (int i = split.length - 1; i >= 2 ; i--) {
-            System.out.print(split[i] + " ");
+        for (int i = split.length - 1; i >= 0 ; i--) {
+            stringBuffer.append(split[i] + " ");
         }
-        if(split[0].length() == 0) {
-            System.out.print(split[1]);
-        } else {
-            System.out.print(split[1] + " " + split[0]);
-        }
+        System.out.println(stringBuffer.toString().trim());
     }
 }
