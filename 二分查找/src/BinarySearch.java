eb3c79c47139c7a687e583cecdc674316c07c467
HHHHH-Y <984495875@qq.com>
2020-08-20 17:14:49 +0800

    实现二分查找

--- 二分查找/src/BinarySearch.java
+++ 二分查找/src/BinarySearch.java
@@ -12,6 +12,9 @@ public class BinarySearch {
         System.out.println(search(nums, target));
     }
     public static int search(int[] nums, int target) {
+        if(nums == null || nums.length == 0) {
+            return -1;
+        }
         int left = 0;
         int right = nums.length - 1;
         while (left <= right) {
