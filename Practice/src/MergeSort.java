e650eb4226a0137afa7f7954f9bda572db3f2921
HHHHH-Y <984495875@qq.com>
2020-09-09 15:45:00 +0800

    代码练习

---
+++ Practice/src/MergeSort.java
@@ -0,0 +1,56 @@
+import java.util.Arrays;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description: 归并排序
+ * User: HHH.Y
+ * Date: 2020-09-09
+ */
+public class MergeSort {
+    public static void main(String[] args) {
+        int[] arr = {10, 6, 7, 1, 3, 9, 4, 5};
+        mergeSort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    private static void mergeSort(int[] arr) {
+        mergeSortInternal(arr, 0, arr.length - 1);
+    }
+
+    private static void mergeSortInternal(int[] arr, int left, int right) {
+        if(left >= right) {
+            return;
+        }
+
+        int mid = (left + right) / 2;
+        mergeSortInternal(arr, left, mid);
+        mergeSortInternal(arr, mid + 1, right);
+
+        merge(arr, left, mid, right);
+    }
+
+    private static void merge(int[] arr, int left, int mid, int right) {
+        int s1 = left;
+        int s2 = mid + 1;
+        int len = right - left + 1;
+        int[] ret = new int[len];
+        int index = 0;
+        while (s1 <= mid && s2 <= right) {
+            if(arr[s1] <= arr[s2]) {
+                ret[index++] = arr[s1++];
+            } else {
+                ret[index++] = arr[s2++];
+            }
+        }
+        while (s1 <= mid) {
+            ret[index++] = arr[s1++];
+        }
+        while (s2 <= right) {
+            ret[index++] = arr[s2++];
+        }
+
+        for (int i = 0; i < ret.length; i++) {
+            arr[i + left] = ret[i];
+        }
+    }
+}
